2129488db35df171529bbadbe8c50ac0744b741a
lomboboo <lomboboo@gmail.com>
2016-12-26 12:54:44 +0100

    make Morgna to be used only on DEVELOPMENT

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require( 'express' );
 const app = express();
 const path = require( 'path' );
 const bodyParser = require( 'body-parser' );
-const morgan = require( "morgan" );
 
 const { config } = require( '../config/server/default' );
 const publicPath = path.join( __dirname, '../frontend' );
@@ -11,7 +10,8 @@ const { userRouter } = require( './routes/user' );
 /***********************************************/
                 /* MIDDLEWARE */
 /***********************************************/
-if ( process.env.NODE_ENV !== 'test' ) {
+if ( process.env.NODE_ENV === 'development' ) {
+  const morgan = require( "morgan" );
   //use morgan to log at command line
   app.use( morgan( 'combined' ) ); //'combined' outputs the Apache style LOGs
 }
